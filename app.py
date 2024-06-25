25927187b7874fef6ee1683c0f37dd8763cf6547
Alexander Larionov <alarionov93@yandex.ru>
2024-06-25 15:28:07 +0500

    added render template

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 # save this as app.py
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, World!"
+    return render_template("base.html", name='Alex', test='test')
 
+@app.route("/books")
+def books():
+    return ['asdfg']
+
+if __name__ == '__main__':
+    app.run(debug=True)
