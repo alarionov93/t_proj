509906c3759de8f74fd885084c34d55457be31a2
Alexander Larionov <alarionov93@yandex.ru>
2024-06-26 12:52:05 +0500

    add login form

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 # save this as app.py
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, current_app, redirect, url_for, request
+from flask_login import login_required, current_user
 
 app = Flask(__name__)
 
@@ -7,9 +8,16 @@ app = Flask(__name__)
 def hello():
     return render_template("base.html", name='Alex', test='test')
 
-@app.route("/books")
-def books():
-    return ['asdfg']
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.args.get('username')
+        password = request.args.get('password')
+        print(username, password)
+        # login user
+        return redirect(url_for('hello'), code=302)
+    else:
+        return render_template("login.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
